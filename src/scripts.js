d2fdfba6c3f1dbcd95410c1c4f39366499576dcc
FireTheLost <prajwalmsreddy@gmail.com>
2021-07-25 20:33:24 +0530

    initial commit

---
+++ src/scripts.js
@@ -0,0 +1,13 @@
+function humor()
+{
+    document.getElementById("about-humor").innerHTML = "CodeX.exe"
+    document.getElementById("img-clutter").src = "file:///C:/Users/prajw/Documents/EH/images/codex-exe.png";
+    document.getElementById("img-clutter").alt = "CodeX.exe";
+}
+
+function unhumor()
+{
+    document.getElementById("about-humor").innerHTML = "We All Know At Least One Person Like This";
+    document.getElementById("img-clutter").src = "file:///C:/Users/prajw/Documents/EH/images/cluttered-desktop.jpg";
+    document.getElementById("img-clutter").alt = "cluttered desktop";
+}
